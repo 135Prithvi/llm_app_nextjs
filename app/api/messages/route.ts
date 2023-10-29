45dfdcaa69c12fdb727b051f95e5d26364a4ebbe
135Prithvi <prithvi128717@gmail.com>
2023-10-29 13:07:14 +0530

    first commit

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,122 +1,119 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { oneLine, stripIndent } from "common-tags";
 import { createClient } from "@supabase/supabase-js";
-import { codeBlock, oneLine, stripIndent } from "common-tags";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { pipeline } from "@xenova/transformers";
-import { HfInference } from "@huggingface/inference";
 
+export const dynamic = "force-dynamic";
 export async function POST(req: Request) {
-  const { message } = await req.json();
-  const db = createServerComponentClient({ cookies });
-  const reqObj = {
-    input: message || "What role does discipline play?",
-  };
-  const embeddedData = await fetch(
-    "https://oinlqhpqjsnaqpssmcek.supabase.co/functions/v1/embed",
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.DENO_SUPABASE_KEY}`,
+  try {
+    const { message } = await req.json();
+    const db = createClient(
+      "https://oinlqhpqjsnaqpssmcek.supabase.co",
+      process.env.DENO_SUPABASE_KEY ?? ""
+    );
+    const reqObj = {
+      input: message || "What role does discipline play?",
+    };
+    const embeddedData = await fetch(
+      "https://oinlqhpqjsnaqpssmcek.supabase.co/functions/v1/embed",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.DENO_SUPABASE_KEY}`,
+        },
+        body: JSON.stringify(reqObj),
+        method: "POST",
+      }
+    );
+    const { embedding: embed } = await embeddedData.json();
+
+    const { data, error } = await db.rpc("match_data_text", {
+      query_embedding: embed,
+      match_threshold: 0.74,
+      match_count: 1,
+    });
+
+    const contextText = data
+      .map((document: { content: any }) => document.content)
+      .join(" ");
+console.log(contextText);
+
+    const apiKey = process.env.API_KEY || "AIzaSyAxdiG_tnvu4cWXxXIvR0IDcy_cociZ2qU";
+
+    const prompt = stripIndent`${oneLine`
+     
+    input:${contextText}${reqObj.input}
+     
+
+      output:
+    `}`;
+
+    const requestBody = {
+      prompt: {
+        text: prompt,
       },
-      body: JSON.stringify(reqObj),
-      method: "POST",
-    }
-  );
-  const { embedding: embed } = await embeddedData.json();
-  // console.log(embed);
+      maxOutputTokens: 1024,
+      temperature: 0.5,
+      candidate_count: 1,
+      top_k: 40,
+      top_p: 0.95,
+      stop_sequences: [],
+      safety_settings: [
+        { category: "HARM_CATEGORY_DEROGATORY", threshold: 4 },
+        { category: "HARM_CATEGORY_TOXICITY", threshold: 4 },
+        { category: "HARM_CATEGORY_VIOLENCE", threshold: 4 },
+        { category: "HARM_CATEGORY_SEXUAL", threshold: 4 },
+        { category: "HARM_CATEGORY_MEDICAL", threshold: 4 },
+        { category: "HARM_CATEGORY_DANGEROUS", threshold: 4 },
+      ],
+    };
 
-  const { data, error } = await db.rpc("match_data_text", {
-    query_embedding: embed,
-    match_threshold: 0.74,
-    match_count: 2,
-  });
-  console.log(data, "matced data");
-  const contextText = data
-    .map((document: { content: any }) => document.content)
-    .join(" ");
-  console.log(contextText, "context data");
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=${apiKey}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      }
+    );
 
-  // const contextText = data[0].content;
-  const apiKey = process.env.API_KEY || ''; // Replace with your actual API key
-  // Answer the question using the context or example of question Answer. Respond with only the text provided.
-  const prompt = stripIndent`${oneLine`
-  Output in markdown format.
-          input:${contextText}
-          
-          ${reqObj.input}
-    
-          In markdown format.
-          output:
-          `}
-        `;
-  const requestBody = {
-    prompt: {
-      text: prompt,
-    },
-    maxOutputTokens: 1024,
-    temperature: 0.5,
-    candidate_count: 1,
-    top_k: 40,
-    top_p: 0.95,
-    stop_sequences: [],
-    safety_settings: [
-      { category: "HARM_CATEGORY_DEROGATORY", threshold: 4 },
-      { category: "HARM_CATEGORY_TOXICITY", threshold: 4 },
-      { category: "HARM_CATEGORY_VIOLENCE", threshold: 4 },
-      { category: "HARM_CATEGORY_SEXUAL", threshold: 4 },
-      { category: "HARM_CATEGORY_MEDICAL", threshold: 4 },
-      { category: "HARM_CATEGORY_DANGEROUS", threshold: 4 },
-    ],
-  };
-
-  // const inference = new HfInference("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");
-  // const out = await inference.textGeneration({
-  //   model: "bigscience/bloom",
-  //   inputs: prompt,
-  // });
-  // console.log(out);
-  const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=${apiKey}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestBody),
-    }
-  );
-  const y = 4;
-  const x = 5;
-  if (response.ok) {
-    // Handle the generated text in the responseData
-    const responseData = await response.json();
-    console.log(responseData); ///end
-    const result = responseData.candidates[0].output;
-    if (responseData.filters) {
+    if (response.ok) {
+      const responseData = await response.json();
+      const result = responseData.candidates[0].output;
+      // if (responseData.filters) {
+      //   return NextResponse.json({
+      //     user: {
+      //       name: "Ai",
+      //       avatar: "https://avatars.githubusercontent.com/u/60410876?s=48&v=4",
+      //     },
+      //     message: "Could not generate text",
+      //   });
+      // }
       return NextResponse.json({
         user: {
           name: "Ai",
           avatar: "https://avatars.githubusercontent.com/u/60410876?s=48&v=4",
         },
-        message: "Could not generate text",
+        message: result,
+      });
+    } else {
+      console.error("Error generating text:", response.status);
+      return NextResponse.json({
+        user: {
+          name: "Ai",
+          avatar: "https://avatars.githubusercontent.com/u/60410876?s=48&v=4",
+        },
+        message: "Error generating text",
       });
     }
-
+  } catch (error) {
+    console.error("An error occurred:", error);
     return NextResponse.json({
       user: {
         name: "Ai",
         avatar: "https://avatars.githubusercontent.com/u/60410876?s=48&v=4",
       },
-      message: result,
-    });
-  } else {
-    console.error("Error generating text:", 422);
-    return NextResponse.json({
-      user: {
-        name: "Ai",
-        avatar: "https://avatars.githubusercontent.com/u/60410876?s=48&v=4",
-      },
-      message: "Error generating text",
+      message: "An error occurred",
     });
   }
 }
--- components/MessageClientComponent.tsx
+++ components/MessageClientComponent.tsx
@@ -3,19 +3,22 @@ import { sendM } from "@/app/_actions/sendM";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
+// import { Textarea } from "@/components/ui/textarea"
 import { experimental_useOptimistic as useOptimistic, useState } from "react";
 import ReactMarkdown from "react-markdown";
+interface Message {
+  user: { name: string; avatar: string }; message: string;
+}
 export default function MessageClientComponent({
   messages,
 }: {
-  messages: Array<{ user: { name: string; avatar: string }; message: string }>;
+  messages: Array<Message>;
 }) {
+  const [message, setMessage] = useState("");
   const [optimisticMessages, addOptimisticMessage] = useOptimistic(
     messages,
-    (state, newMessageObj) => [...state, newMessageObj]
+    (state, newMessageObj) => [...state, newMessageObj as Message]
   );
-  const [message, setMessage] = useState("");
 
   async function sendMessage() {
     const obj = {
@@ -51,10 +54,32 @@ export default function MessageClientComponent({
   return (
     <>
       <div className="relative w-full h-[calc(100vh-64px)]">
-        <div className="sm:container h-full px-2 sm:px-auto overflow-y-auto pb-28">
+        <div className="sm:container h-full px-2 sm:px-auto  overflow-y-auto pb-28">
+          {/* <div className="flex gap-x-2 mt-6 border p-2 rounded-2xl h-auto">
+            <div className="flex items-start ">
+              <Avatar>
+                <AvatarImage src={"item.user?.avatar"} alt="@ai" />
+                <AvatarFallback>{"item.user.name"}</AvatarFallback>{" "}
+              </Avatar>{" "}
+            </div>
+            <Label htmlFor="terms" className="leading-6 font-thin">
+              <ReactMarkdown>
+                Unemployment rate: 🇳🇬 Nigeria: 33.3% 🇿🇦 South Africa: 32.9% 🇮🇶
+                Iraq: 15.55% 🇧🇦 Bosnia and Herzegovina: 13.3% 🇦🇫 Afghanistan:
+                13.3% 🇪🇸 Spain: 11.6% 🇬🇷 Greece: 11.1% 🇺🇦 Ukraine: 10.5% 🇮🇷
+                Iran: 9.7% 🇹🇷 Turkey: 9.5% 🇨🇴 Colombia: 9.34% 🇸🇪 Sweden: 9.2% 🇨🇱
+                Chile: 8.53% 🇺🇾 Uruguay: 8.2%… France just lost another African
+                country, Niger is heading towards multipolar World. Endless
+                humiliation for France and Collective West. It is a clear trend
+                that is accelerating. BRICS, SCO, de-dedollarization, kicking
+                out France from Africa, Saudi Arabia dumping US and… the poorest
+                country
+              </ReactMarkdown>
+            </Label>
+          </div> */}
           {optimisticMessages.map((item) => (
             <div className="flex gap-x-2 mt-6 border p-2 rounded-2xl h-auto">
-              <div className="flex items-center ">
+              <div className="flex items-start ">
                 <Avatar>
                   <AvatarImage src={item.user?.avatar} alt="@ai" />
                   <AvatarFallback>{item.user.name}</AvatarFallback>{" "}
@@ -65,8 +90,6 @@ export default function MessageClientComponent({
               </Label>
             </div>
           ))}
-
-         
         </div>
         <div className="w-full flex grow p-5 sticky -bottom-0">
           <form action={sendMessage} className="w-full grow">
@@ -77,7 +100,7 @@ export default function MessageClientComponent({
               name="message"
               required
               minLength={3}
-              className="rounded-3xl pl-7 dark:bg-[rgb(30,39,50)] dark:focus-visible:ring-[rgb(30,39,50)] grow"
+              className="rounded-3xl pl-7 dark:bg-[rgb(30,39,50)] dark:focus-visible:ring-[rgb(30,39,50)] grow h-12 "
             />
           </form>
         </div>
